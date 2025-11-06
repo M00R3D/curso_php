99f2e1662a3566247cf837a43d13e1c3eb61e058
M00R3D <jobmurdan@hotmail.com>
2025-11-05 22:32:53 -0700

    funciones y comentarios

--- index.php
+++ index.php
@@ -7,13 +7,19 @@
 </head>
 <body>
     <?php 
+    function threeRandomNumbers($min, $max) {
+        return rand($min, $max) . ", " . rand($min, $max) . ", " . rand($min, $max);
+    }
+
     $var1 = "this is var1";
     $var2 = "this is var2";
     $var3 = "this is var3";
 
-    echo $var1 . $var2 . $var3; 
-    print "<br>";
-    print $var1 . " " . $var2 . " " . $var3;
+    // echo $var1 . $var2 . $var3; 
+    /* print "<br>";
+     print $var1 . " " . $var2 . " " . $var3; */
+
+    echo threeRandomNumbers(1, 100);
     ?>
 </body>
 </html>
