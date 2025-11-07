aafcd87549c3063fb1914ac4899b91ab30640a2c
M00R3D <jobmurdan@hotmail.com>
2025-11-07 07:03:20 -0700

    variable local

--- index.php
+++ index.php
@@ -7,19 +7,27 @@
 </head>
 <body>
     <?php 
-    function threeRandomNumbers($min, $max) {
-        return rand($min, $max) . ", " . rand($min, $max) . ", " . rand($min, $max);
-    }
+    // function threeRandomNumbers($min, $max) {
+        // return rand($min, $max) . ", " . rand($min, $max) . ", " . rand($min, $max);
+    // }
 
-    $var1 = "this is var1";
-    $var2 = "this is var2";
-    $var3 = "this is var3";
+    // $var1 = "this is var1";
+    // $var2 = "this is var2";
+    // $var3 = "this is var3";
 
     // echo $var1 . $var2 . $var3; 
     /* print "<br>";
      print $var1 . " " . $var2 . " " . $var3; */
 
-    echo threeRandomNumbers(1, 100);
+    // echo threeRandomNumbers(1, 100);
+
+    $varLocal = "this is a local variable";
+        function testScope() {
+            $varLocal = "this is a local variable inside the function";
+        }
+    testScope();
+    //a pesar de llamar a la funcion, la variable local no cambia su valor fuera de la funcion
+    echo $varLocal;
     ?>
 </body>
 </html>
