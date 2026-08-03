07ee5bc95d88bacc74faa49dfceffd4b857ba757
M00R3D <jobmurdan@hotmail.com>
2026-08-02 20:28:53 -0700

    Curso PHP MySql. Sistema de login II. Vídeo 60

--- login/comprueba_login.php
+++ login/comprueba_login.php
@@ -4,12 +4,13 @@ require ("getUsuarios.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST["username"];
     $contrasena = $_POST["password"];
-
     $getUsuarios = new getUsuarios();
     $usuario = $getUsuarios->comprobar_usuario($nombre, $contrasena);
-
+    
     if ($usuario) {
-        echo "Inicio de sesión exitoso";
+        session_start();
+        $_SESSION["usuario"] = $usuario;
+        header ("Location: index.php");
     } else {
         echo "Nombre o contraseña incorrectos";
     }
