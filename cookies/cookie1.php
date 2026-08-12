42318836acda86c0adc4002c1061a2317fb817df
M00R3D <jobmurdan@hotmail.com>
2026-08-12 04:47:19 -0700

    como borrar una cookie

--- cookies/cookie1.php
+++ cookies/cookie1.php
@@ -7,7 +7,9 @@
 </head>
 <body>
     <?php
-        setcookie("prueba", "esta es la información de la cookieeeeeeee", time() + 3600, "/");
+        setcookie("prueba", "esta es la información de la cookieeeeeeee", time() + 50, "/");
+        // setcookie("prueba", "esta es la información de la cookieeeeeeee", time() -1, "/");
+        //agregar el time-1 es la manera de eliminar la cookie, ya que le estamos dando un tiempo negativo, por lo que el navegador la elimina
     ?>
 </body>
 </html>
